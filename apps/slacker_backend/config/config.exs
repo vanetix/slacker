12f426919fe609f50bfb7a406ad82e1b4cef3750
Matt McFarland <783024+vanetix@users.noreply.github.com>
2018-04-04 17:54:13 -0500

    Update libcluster configuration to kubernetes

--- apps/slacker_backend/config/config.exs
+++ apps/slacker_backend/config/config.exs
@@ -32,15 +32,10 @@ use Mix.Config
 config :libcluster,
   topologies: [
     cluster: [
-      strategy: Cluster.Strategy.Gossip,
+      strategy: Cluster.Strategy.Kubernetes,
       config: [
-        port: 45892,
-        if_addr: {0,0,0,0},
-        multicast_addr: {230,1,1,251},
-        # a TTL of 1 remains on the local network,
-        # use this to change the number of jumps the
-        # multicast packets will make
-        multicast_ttl: 1
+        kubernetes_selector: "app=slacker",
+        kubernetes_node_basename: "slacker"
       ]
     ]
   ]
